f9941940a11a8559fed802d032fcc700eb3a6c41
Balades <haryoholuwah1@gmail.com>
2023-07-03 03:15:30 +0100

    please

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import leftarr from './arrow-circle-left.svg'
+import rightarr from './arrow-circle-right.svg'
+import barbercutting from './barbercutting.png'
+import google from './google-icon-undefined.png'
+import facebook from './facebook-icon-undefined.png'
+import instagram from './instagram-icon-undefined.png'
+
+export default {
+    leftarr, rightarr,barbercutting,google, instagram, facebook
+}
